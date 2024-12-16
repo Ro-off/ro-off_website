658763bcf295d61400a33e55a866a255dfd59ea8
Ro_off <romansk2001@gmail.com>
2024-12-16 23:49:41 +0200

    Refactor IndexPage to include ChipList, LogoGallery, and ProjectsGallery components for enhanced skills and projects display

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,39 @@
 import { title, subtitle } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+import { ChipList } from "@/components/chip-list";
+import { ProjectsGallery } from "@/components/projects-gallery";
+import { LogoGallery } from "@/components/logo-gallery";
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <span className={title()}>Make&nbsp;</span>
-          <span className={title({ color: "violet" })}>beautiful &nbsp;</span>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 h-screen">
+        <div className="inline-block text-center justify-center">
+          <span className={title()}>Roman&nbsp;</span>
+          <span className={title()}>Sklyarov &nbsp;</span>
           <br />
-          <span className={title()}>
-            websites regardless of your design experience.
-          </span>
-          <div className={subtitle({ class: "mt-4" })}>
-            Beautiful, fast and modern React UI library 1.
-          </div>
-          <div className={subtitle({ class: "mt-4" })}>
-            Beautiful, fast and modern React UI library 1.
-          </div>
+          <div className={subtitle({ class: "mt-4" })}>Web Developer</div>
+          <br />
+          <ChipList
+            data={[
+              "HTML",
+              "CSS",
+              "JavaScript",
+              "React",
+              "TypeScript",
+              "TailwindCSS",
+            ]}
+          />
+          <br />
         </div>
+      </section>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 min-h-screen">
+        <p className={`${title({ color: "blue" })} m-3`}>Skills</p>
+        <LogoGallery />
+      </section>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 min-h-screen">
+        <p className={`${title({ color: "blue" })} m-3`}>Projects</p>
+        <ProjectsGallery />
       </section>
     </DefaultLayout>
   );
