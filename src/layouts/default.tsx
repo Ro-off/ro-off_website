f6c60ea61f282f618096929af568c9ce8960f65c
Ro_off <romansk2001@gmail.com>
2024-12-16 17:33:30 +0200

    Update layout to use min-h-screen for better responsiveness

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -4,7 +4,7 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex flex-col h-screen">
+    <div className="relative flex flex-col min-h-screen">
       <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
         {children}
       </main>
