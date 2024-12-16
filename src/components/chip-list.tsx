77773c716a13f294d2df4b7381213321456cce7f
Ro_off <romansk2001@gmail.com>
2024-12-16 17:34:08 +0200

    Add ChipList component for rendering a list of chips

---
+++ src/components/chip-list.tsx
@@ -0,0 +1,21 @@
+import { Chip, ChipProps } from "@nextui-org/chip";
+
+export function ChipList({
+  data,
+  ChipProps,
+}: {
+  data: string[];
+  ChipProps?: ChipProps;
+}) {
+  return (
+    <>
+      <div className="flex flex-wrap justify-center gap-2">
+        {data.map((label) => (
+          <Chip key={label} {...ChipProps}>
+            {label}
+          </Chip>
+        ))}
+      </div>
+    </>
+  );
+}
