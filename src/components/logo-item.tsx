fa30622f2fdf28b57437d108194df68ef56c7789
Ro_off <romansk2001@gmail.com>
2024-12-17 19:30:43 +0200

    Refactor LogoItem component to use NextUI Image and comment out random rotation logic for improved performance

--- src/components/logo-item.tsx
+++ src/components/logo-item.tsx
@@ -1,10 +1,11 @@
 import seedrandom from "seedrandom";
 import { motion } from "framer-motion";
+import { Image } from "@nextui-org/image";
 
-function generateRandomRotation(seed: string) {
-  const rng = seedrandom(seed + "f");
-  return rng() * 60 - 30;
-}
+// function generateRandomRotation(seed: string) {
+//   const rng = seedrandom(seed + "f");
+//   return rng() * 60 - 30;
+// }
 
 function generateRandomScale(seed: string) {
   const rng = seedrandom(seed + "s");
@@ -12,27 +13,29 @@ function generateRandomScale(seed: string) {
 }
 
 export function LogoItem({ src, alt }: { src: string; alt: string }) {
-  const rotation = generateRandomRotation(src);
+  //const rotation = generateRandomRotation(src);
   return (
     <motion.div
       className={`flex items-center justify-center`}
       initial={{
-        rotate: rotation,
+        //rotate: rotation,
         scale: generateRandomScale(src),
         zIndex: 10,
         y: 50,
       }}
       whileHover={{ rotate: 0, scale: 1.2, zIndex: 14 }}
       whileInView={{
-        rotate: generateRandomRotation(src),
+        //rotate: generateRandomRotation(src),
         y: 0,
         scale: 1,
       }}
     >
-      <img
+      <Image
         src={src}
         alt={alt}
-        className="w-40 h-40 drop-shadow-lg cursor-pointer"
+        className=" h-40 cursor-pointer"
+        isBlurred
+        // isZoomed
       />
     </motion.div>
   );
