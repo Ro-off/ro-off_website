6c0d8739324593f95559f01007901512e1f534cd
Ro_off <romansk2001@gmail.com>
2024-12-17 19:30:55 +0200

    Remove top padding from main element for improved layout consistency

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -5,7 +5,7 @@ export default function DefaultLayout({
 }) {
   return (
     <div className="relative flex flex-col min-h-screen">
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+      <main className="container mx-auto max-w-7xl px-6 flex-grow ">
         {children}
       </main>
     </div>
