97978fe5dbfa5cdc3d5b85ec2bdadaff4e39a2f0
Ro_off <romansk2001@gmail.com>
2024-12-17 19:32:03 +0200

    Refactor IndexPage layout and styling for improved readability and presentation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,32 +7,28 @@ import { LogoGallery } from "@/components/logo-gallery";
 export default function IndexPage() {
   return (
     <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 h-screen">
-        <div className="inline-block text-center justify-center">
-          <span className={title()}>Roman&nbsp;</span>
-          <span className={title()}>Sklyarov &nbsp;</span>
+      <section className="flex flex-col justify-center gap-32 py-8 md:py-10 h-screen">
+        <div className="block text- justify-center">
+          <span className={" font-bold text-9xl text-right "}>
+            Hi there&nbsp;
+          </span>
           <br />
-          <div className={subtitle({ class: "mt-4" })}>Web Developer</div>
+          <span className={" font-bold text-9xl"}>I am&nbsp;</span>
+          <span className={" font-bold text-9xl text-blue-500"}>
+            Roman&nbsp;
+          </span>
           <br />
-          <ChipList
-            data={[
-              "HTML",
-              "CSS",
-              "JavaScript",
-              "React",
-              "TypeScript",
-              "TailwindCSS",
-            ]}
-          />
+          <span className={" font-bold text-9xl"}>Front End&nbsp;</span>
           <br />
+          <span className={" font-bold text-9xl"}>Developer&nbsp;</span>
         </div>
       </section>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 min-h-screen">
-        <p className={`${title({ color: "blue" })} m-3`}>Skills</p>
+      <section className="flex flex-col items-center  gap-4 py-8 md:py-10 min-h-screen">
+        <p className={"m-32 font-bold text-9xl"}>Skills</p>
         <LogoGallery />
       </section>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 min-h-screen">
-        <p className={`${title({ color: "blue" })} m-3`}>Projects</p>
+        <p className={"m-32 font-bold text-9xl"}>Projects</p>
         <ProjectsGallery />
       </section>
     </DefaultLayout>
