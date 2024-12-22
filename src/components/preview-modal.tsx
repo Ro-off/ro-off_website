2ada4bc76c79e0f28379d9e0fb52da2bbdab446a
Ro_off <romansk2001@gmail.com>
2024-12-22 19:51:48 +0200

    Add PreviewModal component and integrate with ProjectsGallery for enhanced project previews

---
+++ src/components/preview-modal.tsx
@@ -0,0 +1,80 @@
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+} from "@nextui-org/modal";
+import { Button } from "@nextui-org/button";
+import { ProjectRecord } from "@/types";
+import { Skeleton } from "@nextui-org/skeleton";
+import { CodeIcon } from "@/icons/code-icon";
+import { FullscreenIcon } from "@/icons/fullscreen";
+
+export function PreviewModal({
+  isOpen,
+  onOpenChange,
+  data,
+}: {
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+  data: ProjectRecord["value"][0];
+}) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+      size="5xl"
+      backdrop="blur"
+      className="dark text-foreground"
+    >
+      <ModalContent>
+        {() => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">
+              <h2 className="text-2xl font-bold">{data.title}</h2>
+              <p className="text-sm text-gray-500">{data.description}</p>
+            </ModalHeader>
+            <ModalBody className="relative px-0 mx-4">
+              <Skeleton className="rounded-lg w-full h-[calc(85vh-200px)] absolute z-30 right-0" />
+              {data.previewSrc ? (
+                <iframe
+                  className="w-full h-[calc(85vh-200px)] rounded-lg z-40 object-scale-down"
+                  src={data.previewSrc}
+                  title={data.title}
+                />
+              ) : (
+                <img
+                  className="w-full h-[calc(85vh-200px)] rounded-lg z-40 object-cover"
+                  src={data.imageSrc}
+                  alt={data.title}
+                />
+              )}
+            </ModalBody>
+            <ModalFooter>
+              <Button
+                onPress={() => window.open(data.sourceSrc)}
+                variant="light"
+                size="lg"
+              >
+                <CodeIcon />
+                <span>Open source code</span>
+              </Button>
+              {data.previewSrc && (
+                <Button
+                  isIconOnly
+                  onPress={() => window.open(data.previewSrc ?? "")}
+                  variant="shadow"
+                  color="primary"
+                  size="lg"
+                >
+                  <FullscreenIcon />
+                </Button>
+              )}
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+}
--- src/components/projects-gallery.tsx
+++ src/components/projects-gallery.tsx
@@ -3,47 +3,57 @@ import { PreviewIcon } from "@/icons/preview-icon";
 import { CodeIcon } from "@/icons/code-icon";
 import { Spinner } from "@nextui-org/spinner";
 import { ProjectRecord } from "@/types";
+import { useDisclosure } from "@nextui-org/modal";
+import { PreviewModal } from "@/components/preview-modal";
+import { useState } from "react";
 
 export function ProjectsGallery({ data }: { data: ProjectRecord }) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [modalData, setModalData] = useState<ProjectRecord["value"][0] | null>(
+    null
+  );
+
+  function handleOpenModal(data: ProjectRecord["value"][0]) {
+    setModalData(data);
+    onOpen();
+  }
+
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {data.isLoading || !data ? (
-        <Spinner size="lg" />
-      ) : (
-        data.value.map((item) => (
-          <CardItem
-            key={item.id}
-            title={item.title}
-            description={item.description}
-            CardProps={{
-              isPressable: true,
-              onPress: () => {
-                if (item.previewSrc) {
-                  window.open(item.previewSrc, "_blank");
-                } else {
-                  window.open(item.sourceSrc, "_blank");
-                }
-              },
-            }}
-            src={item.imageSrc}
-            hoverContent={
-              item.previewSrc ? (
+    <>
+      {modalData && (
+        <PreviewModal
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+          data={modalData}
+        />
+      )}
+
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {data.isLoading || !data ? (
+          <Spinner size="lg" />
+        ) : (
+          data.value.map((item) => (
+            <CardItem
+              key={item.id}
+              title={item.title}
+              description={item.description}
+              CardProps={{
+                isPressable: true,
+                onPress: () => handleOpenModal(item),
+              }}
+              src={item.imageSrc}
+              hoverContent={
                 <>
                   <PreviewIcon />
                   <span>View in preview</span>
                 </>
-              ) : (
-                <>
-                  <CodeIcon />
-                  <span>Open source code</span>
-                </>
-              )
-            }
-            chipList={item.techList}
-            className="max-w-full"
-          />
-        ))
-      )}
-    </div>
+              }
+              chipList={item.techList}
+              className="max-w-full"
+            />
+          ))
+        )}
+      </div>
+    </>
   );
 }
